48784916fb537af1a94506551372e2a174ddf1d2
Gianluca Atzeni <gianluca.atzeni@avanade.com>
2025-08-20 15:31:23 +0200

    Manage share URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,107 +1,125 @@
 import { useEffect, useState } from 'react'
 import * as microsoftTeams from "@microsoft/teams-js";
 import './App.css'
-import { FluentProvider,  teamsLightTheme } from '@fluentui/react-components';
+import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
+import { Share24Regular, Checkmark24Filled } from '@fluentui/react-icons';
 import { logo } from './assets/images';
 import { SoundBtn } from './SoundBtn';
 
 
 function App() {
   const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8080";
-  const [meetingUrl, setMeetingUrl] = useState<string>("");
-  const [bot, setBot]=useState<any>(null);
-  const [sounds, setSounds] = useState<any[]>()
-  const [status, setStatus]=useState("")
   const [activeSound, setActiveSound] = useState<string | null>(null);
-  
+  const [bot, setBot] = useState<any>(null);
+  const [copied, setCopied] = useState<boolean>(false);
+  const [meetingUrl, setMeetingUrl] = useState<string>((new URLSearchParams(window.location.search)).get("meeting") || "");
+  const [sounds, setSounds] = useState<any[]>();
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+
+  });
+
   useEffect(() => {
     microsoftTeams.app.initialize().then(() => {
       microsoftTeams.app.getContext().then(context => {
         console.log("Teams context:", context);
-        const meetingId=atob(context?.meeting?.id??"").replace("#0", "").replace("0#", "");
-        const userId=context?.user?.id??"";
-        const tenantId=context?.user?.tenant?.id??"";
-        const uri=`https://teams.microsoft.com/l/meetup-join/${meetingId}/0?context=%7b%22Tid%22%3a%22${tenantId}%22%2c%22Oid%22%3a%22${userId}%22%7d`;
+        const meetingId = atob(context?.meeting?.id ?? "").replace("#0", "").replace("0#", "");
+        const userId = context?.user?.id ?? "";
+        const tenantId = context?.user?.tenant?.id ?? "";
+        const uri = `https://teams.microsoft.com/l/meetup-join/${meetingId}/0?context=%7b%22Tid%22%3a%22${tenantId}%22%2c%22Oid%22%3a%22${userId}%22%7d`;
         setMeetingUrl(uri);
       });
     });
-}, []);
+  }, []);
 
   useEffect(() => {
-    setInterval(async() => {
-      if(bot){
+    setInterval(async () => {
+      if (bot) {
         console.log("Checking bot status...");
-        const b=await fetch(`${apiUrl}/status?meeting_url=${meetingUrl}`).then(response => response.json())
-        if(b)
+        const b = await fetch(`${apiUrl}/status?meeting_url=${meetingUrl}`).then(response => response.json())
+        if (b)
           setStatus(b.status)
-        else{
+        else {
           setStatus("")
           setBot(null);
         }
       }
-    },5000)
+    }, 5000)
   }, [bot]);
 
-  const handleInviteBot = async() => {
-    const bot=await fetch(`${apiUrl}/invite?meeting_url=${meetingUrl}`).then(response => response.json())
+  const handleInviteBot = async () => {
+    const bot = await fetch(`${apiUrl}/invite?meeting_url=${meetingUrl}`).then(response => response.json())
     setBot(bot.Id);
     setStatus(bot.status);
     setSounds(bot.soundsCatalog || []);
   }
 
-  const handlePlaySound=async(id:string)=>{
+  const handlePlaySound = async (id: string) => {
     setActiveSound(id);
     await fetch(`${apiUrl}/play?soundId=${id}&botId=${bot}`)
     setTimeout(() => {
       setActiveSound(null);
     }, 5000); // Simulate a delay for the sound to play
-    
   }
 
   return (
     <FluentProvider theme={teamsLightTheme}>
-      
-      <div style={{display:'flex', flexDirection:"column", alignItems:"center", justifyContent:"center", gap:10, marginTop:20}}>
-                <img style={{width: '150px', height: '150px'}} src={logo} alt="" />
-                <span className='app-title'>MIDI4Meeting</span>
-                <div className="container">
-                  <input
-                  type="text"
-                  placeholder="Meeting Url"
-                  className="meeting-url-input"
-                  value={meetingUrl}
-                  onChange={(ev)=>{setBot(null); setMeetingUrl(ev.currentTarget.value);}}
-                  />
-                  <button
-                  className={`invite-btn ${status}`}
-                  onClick={handleInviteBot}
-                  disabled={status!=""}
-                  >
-                    {(!status || status=="") && "Join"}
-                    {status=="pending" && "Pending"}
-                    {status=="joining" && "Joining"}
-                    {status=="lobby" && "In Lobby"}
-                    {status=="ready" && "In Call"}
-                  </button>
-                </div>
-      {
-      status && status!="" && sounds && sounds.length>0 &&
-      <div className='container' style={{display:'flex', flexDirection:"row", flexWrap:"wrap", justifyContent:"start", padding:"8px", gap:"8px"}}>
-          {sounds && sounds.map((sound, index) => (
-            <SoundBtn
-              key={index}
-              sound={sound}
-              onPlay={handlePlaySound}
-              disabled={!bot || status!="ready"}
-              active={activeSound === sound.id}
-            />
-            )
-          )}
+
+      <div style={{ display: 'flex', flexDirection: "column", alignItems: "center", justifyContent: "center", gap: 10, marginTop: 20 }}>
+        <img style={{ width: '150px', height: '150px' }} src={logo} alt="" />
+        <span className='app-title'>MIDI4Meeting</span>
+        <div className="container">
+          <input
+            type="text"
+            placeholder="Meeting Url"
+            className="meeting-url-input"
+            value={meetingUrl}
+            onChange={(ev) => { setBot(null); setMeetingUrl(ev.currentTarget.value); }}
+          />
+          <button
+            className={`invite-btn ${status}`}
+            onClick={handleInviteBot}
+            disabled={status != ""}
+          >
+            {(!status || status == "") && "Join"}
+            {status == "pending" && "Pending"}
+            {status == "joining" && "Joining"}
+            {status == "lobby" && "In Lobby"}
+            {status == "ready" && "In Call"}
+          </button>
+          <button>
+            {!copied ? (
+              <Share24Regular onClick={() => {
+                const currentUrl: string = window.location.href;
+                navigator.clipboard.writeText(`${currentUrl}?meeting=${meetingUrl}`);
+                setCopied(true);
+              }} />
+            ) : (
+              <Checkmark24Filled 
+                onClick={() => {setCopied(false)}}
+              />
+            )}
+          </button>
         </div>
+        {
+          status && status != "" && sounds && sounds.length > 0 &&
+          <div className='container' style={{ display: 'flex', flexDirection: "row", flexWrap: "wrap", justifyContent: "start", padding: "8px", gap: "8px" }}>
+            {sounds && sounds.map((sound, index) => (
+              <SoundBtn
+                key={index}
+                sound={sound}
+                onPlay={handlePlaySound}
+                disabled={!bot || status != "ready"}
+                active={activeSound === sound.id}
+              />
+            )
+            )}
+          </div>
         }
       </div>
     </FluentProvider>
-)
+  )
 }
 
 export default App
--- client/src/SoundBtn.tsx
+++ client/src/SoundBtn.tsx
@@ -1,4 +1,4 @@
-export interface ISound{
+export interface ISound {
     name: string;
     id: string;
     imgUrl: string;
@@ -14,37 +14,37 @@ export interface ISoundBtnProps {
 
 export const SoundBtn = ({ sound, onPlay, disabled, active }: ISoundBtnProps) => {
 
-    const handlePlay=(id:string)=>{
+    const handlePlay = (id: string) => {
         onPlay(id);
     }
-  return (
-    <div className='sound-btn' onClick={()=> {if(!disabled)handlePlay(sound.id)}}>
-        <div style={{width:"100%",display:'flex', flexDirection:"row-reverse", alignItems:"center",justifyContent:"space-between"}}>
-            {/* <button className={`invite-btn play`} onClick={() => handlePlay(sound.id)} disabled={disabled}>Play</button> */}
-            {/* <div style={{width:"inherit", height:"40px", overflow:"hidden", borderRadius:"8px"}}>
+    return (
+        <div className='sound-btn' onClick={() => { if (!disabled) handlePlay(sound.id) }}>
+            <div style={{ width: "100%", display: 'flex', flexDirection: "row-reverse", alignItems: "center", justifyContent: "space-between" }}>
+                {/* <button className={`invite-btn play`} onClick={() => handlePlay(sound.id)} disabled={disabled}>Play</button> */}
+                {/* <div style={{width:"inherit", height:"40px", overflow:"hidden", borderRadius:"8px"}}>
             <img
                 style={{width:"100%", height:"100%", objectFit:"cover"}}
                 src={sound.imgUrl}
                 alt=""
             />
             </div> */}
-        </div>
-        <div style={{width:"100%", display:'flex', flexDirection:"row", alignItems:"center", justifyContent:"space-between", fontSize:"10px", fontWeight:300}}>
-            {sound.name}
-            <div>
-                <span
-                    className={`status-indicator ${active ? 'enabled' : ''}`}
-                    style={{
-                       
-                    }}
-                />
             </div>
-            {/* <img
+            <div style={{ width: "100%", display: 'flex', flexDirection: "row", alignItems: "center", justifyContent: "space-between", fontSize: "10px", fontWeight: 300 }}>
+                {sound.name}
+                <div>
+                    <span
+                        className={`status-indicator ${active ? 'enabled' : ''}`}
+                        style={{
+
+                        }}
+                    />
+                </div>
+                {/* <img
                 style={{width:"50px", height:50, objectFit:"cover"}}
                 src={sound.imgUrl}
                 alt=""
             /> */}
-        </div>   
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
